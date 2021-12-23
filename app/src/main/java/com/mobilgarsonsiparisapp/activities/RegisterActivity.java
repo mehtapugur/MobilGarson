a83b725e3c6f1810ad3818cbe154f3d508dfbbd4
Mehtap Ugur <yaziliminval@hotmail.com>
2021-12-23 22:06:57 +0300

    Update RegisterActivity.java

--- app/src/main/java/com/mobilgarsonsiparisapp/activities/RegisterActivity.java
+++ app/src/main/java/com/mobilgarsonsiparisapp/activities/RegisterActivity.java
@@ -4,139 +4,90 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mobilgarsonsiparisapp.R;
-import com.mobilgarsonsiparisapp.model.User;
+import com.mobilgarsonsiparisapp.data.Constanst;
+import com.mobilgarsonsiparisapp.model.Meal;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class OrderActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button btn_KayitOl;
-    private EditText edt_AdiKayitOl, edt_soyadiKayitOl,edt_emailKayitOl,edt_sifreKayitOl;
-    private FirebaseAuth mAuth;
+    ImageView img_detailImage;
+    TextView txt_detailName,txt_detailPrice;
+    Button btn_islemiTamamla;
+    EditText edt_detailAdres;
+    //database
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        //init fonk calistiracak
+        setContentView(R.layout.activity_order);
+        //init fonksiyonu calistiracak
         init();
     }
 
     private void init(){
-        mAuth = FirebaseAuth.getInstance();
-        btn_KayitOl = findViewById(R.id.btn_KayitOl);
-        edt_AdiKayitOl = findViewById(R.id.edt_adiKayitOl);
-        edt_soyadiKayitOl = findViewById(R.id.edt_soyadiKayitOl);
-        edt_emailKayitOl = findViewById(R.id.edt_mailKayitOl);
-        edt_sifreKayitOl = findViewById(R.id.edt_sifreKayitOl);
-        btn_KayitOl.setOnClickListener(this);
+        img_detailImage = findViewById(R.id.img_detailImage);
+        txt_detailName = findViewById(R.id.txt_detailName);
+        txt_detailPrice = findViewById(R.id.txt_detailPrice);
+        edt_detailAdres = findViewById(R.id.edt_detailAdres);
+        btn_islemiTamamla = findViewById(R.id.btn_islemiTamamla);
+        edt_detailAdres = findViewById(R.id.edt_detailAdres);
+        btn_islemiTamamla.setOnClickListener(this);
+
+        //firebase teki Meals ile calisacak
+        ref = FirebaseDatabase.getInstance().getReference().child("Meals");
+
+        ref.child(Constanst.buyItem).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Meal mealkey = snapshot.getValue(Meal.class);
+                txt_detailName.setText(mealkey.getName());
+                txt_detailPrice.setText(mealkey.getPrice());
+                byte[] bytes = Base64.decode(mealkey.getImgURL(),Base64.DEFAULT);
+                Bitmap bitmap2= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                img_detailImage.setImageBitmap(bitmap2);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
-    //kayit yap fonksiyonu
-    public void kayitYap(){
-        //adi alacak
-        String adi = edt_AdiKayitOl.getText().toString().trim();
-        //soyadi alacak
-        String soyadi = edt_soyadiKayitOl.getText().toString().trim();
-        //mail alacak
-        String email = edt_emailKayitOl.getText().toString().trim();
-        //sifre alacak
-        String sifre = edt_sifreKayitOl.getText().toString().trim();
-
-        //eger kullanici girdigi adi bos ise
-        if (adi.isEmpty()){
-            //uyari verir
-            edt_AdiKayitOl.setError("Adi boş bırakılamaz");
-            edt_AdiKayitOl.requestFocus();
-            return;
-        }
-
-        //eger soyadi bos ise
-        if (soyadi.isEmpty()){
-            //uyari verir
-            edt_soyadiKayitOl.setError("Soyadi boş bırakılamaz");
-            edt_soyadiKayitOl.requestFocus();
-            return;
-        }
-
-        //mail bos ise
-        if (email.isEmpty()){
-            //uyari verir
-            edt_emailKayitOl.setError("Email boş bırakılamaz");
-            edt_emailKayitOl.requestFocus();
-            return;
-        }
-
-        //sifre bos ise
-        if (sifre.isEmpty()){
-            //uyari verir
-            edt_sifreKayitOl.setError("Sifre boş bırakılamaz");
-            edt_sifreKayitOl.requestFocus();
-            return;
-        }
-
-        //sifrenin uzunlugu 6 dan daha az ise
-        if (sifre.length() <6){
-            //uyari verir
-            edt_sifreKayitOl.setError("Parola 6 haneden uzun olmalıdır");
-            edt_sifreKayitOl.requestFocus();
-            return;
-        }
-        mAuth.createUserWithEmailAndPassword(email, sifre)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        //eger her seh dogru ise
-                        if (task.isSuccessful()){
-                            //firebase teki User kismina kullanicinin girdigi bilgileri ekleyecek
-                            User user = new User(adi,soyadi,email);
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()){
-                                        //eger kullanici firebase basarili bir sekilde eklenmis ise
-                                        Toast.makeText(RegisterActivity.this, "Kullanıcı başarılı bir şekilde eklendi", Toast.LENGTH_SHORT).show();
-                                    }
-                                    else{
-                                        //firebase eklenmemis ise
-                                        Toast.makeText(RegisterActivity.this, "Kullanıcı Eklenemedi", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                        }else{
-                            Toast.makeText(RegisterActivity.this, "Kullanıcı Eklenemedi", Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-
-        //sonraki LoginActivity olacak
-        Intent nextPageActivity=new Intent(getApplicationContext(),LoginActivity.class);
-        //onu ac
+    //Satin alma fonksiyonu
+    public void order(){
+        //uyari yap
+        Toast.makeText(OrderActivity.this, "Satın Alma İşlemi Tamamlandı", Toast.LENGTH_SHORT).show();
+        //sonraki CategoryListingActivity olacak
+        Intent nextPageActivity=new Intent(getApplicationContext(),CategoryListingActivity.class);
+        //onu calisitracak
         startActivity(nextPageActivity);
         finish();
-
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            //kayitYap fonk calistir
-            case R.id.btn_KayitOl:
-                kayitYap();
+            //order fonk calistiracak
+            case R.id.btn_islemiTamamla:
+                order();
                 break;
         }
     }
